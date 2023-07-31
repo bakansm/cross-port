7085d637ce7d1ea3b5ddbfb21ae1db5e842d8f86
bakansm <khuynhbao5@gmail.com>
2023-07-31 16:27:21 +0700

    Add request loading stage

--- faucet/pages/index.tsx
+++ faucet/pages/index.tsx
@@ -1,12 +1,14 @@
+import LoadingSpinner from '@/components/LoaddingSpinner';
 import axios from 'axios';
 import React, { ChangeEvent, FormEvent, useState } from 'react'
 
 export default function Home() {
   const [walletAddress, setWalletAddress] = useState<string>('')
+  const [isLoading, setIsLoading] = useState<boolean>(false)
 
   const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault()
-
+    setIsLoading(true)
     const config = {
       method: 'post',
       maxBodyLength: Infinity,
@@ -15,9 +17,11 @@ export default function Home() {
         
     axios.request(config)
       .then((response) => {
+        setIsLoading(false)
         alert(response.data.message);
       })
       .catch((error) => {
+        setIsLoading(false)
         alert(error);
       });
   }
@@ -29,33 +33,33 @@ export default function Home() {
     <main
     className="flex h-screen w-screen items-center justify-center bg-white"
     >
-      <div className="flex flex-col space-y-4 rounded-2xl bg-slate-50 p-10">
-      <div className="space-y-4">
-        <p className=" text-center text-4xl font-bold text-black">
-          CrossTech Testnet Faucet
-        </p>
-        <p className=" text-center text-xl text-slate-500">
-          Use this form to request tokens to your CrossTech address.
-        </p>
-      </div>
-      <div className="divide-x divide-dashed divide-gray-300"></div>
-      <div className="flex flex-col space-y-4">
-        <form onSubmit={handleSubmit} className="w-full space-y-4">
-          <input
-            type="text"
-            name="walletAddress"
-            placeholder="Enter your CrossTech Address"
-            className=" w-full rounded-full bg-slate-200 p-4 text-black focus:bg-white focus:outline-none"
-            onChange={handleChange}
-            value={walletAddress} 
-          />
-          <button type="submit" className="rounded-full w-full bg-slate-600 px-64 py-4 hover:bg-slate-400">
-          Request Tokens
-        </button>
-        </form>
-        
-      </div>
-    </div>
+      {isLoading ? (<LoadingSpinner text='Requesting'/>) : (<div className="flex flex-col space-y-4 rounded-2xl bg-slate-50 p-10">
+        <div className="space-y-4">
+          <p className=" text-center text-4xl font-bold text-black">
+            CrossTech Testnet Faucet
+          </p>
+          <p className=" text-center text-xl text-slate-500">
+            Use this form to request tokens to your CrossTech address.
+          </p>
+        </div>
+        <div className="divide-x divide-dashed divide-gray-300"></div>
+        <div className="flex flex-col space-y-4">
+          <form onSubmit={handleSubmit} className="w-full space-y-4">
+            <input
+              type="text"
+              name="walletAddress"
+              placeholder="Enter your CrossTech Address"
+              className=" w-full rounded-full bg-slate-200 p-4 text-black focus:bg-white focus:outline-none"
+              onChange={handleChange}
+              value={walletAddress} 
+            />
+            <button type="submit" className="rounded-full w-full bg-slate-600 px-64 py-4 hover:bg-slate-400">
+            Request Tokens
+          </button>
+          </form>
+          
+        </div>
+      </div>)}
     </main>
   )
 }
